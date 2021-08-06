e3bf4d3c63cf037cb1b5e97bbae2c266c06ee80d
Suneet Misra <hello@suneet.codes>
2021-08-06 23:27:47 +0100

    added some custom rules

--- index.js
+++ index.js
@@ -13,5 +13,8 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off",
+  },
 };
